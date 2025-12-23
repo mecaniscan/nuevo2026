ea2c17bf7bbcb39c0a134018f8cb098e45a93b3f
Mecani Scan <mecaniscan50@gmail.com>
2025-12-23 02:45:07 +0000

    Try fixing this error: `Runtime ReferenceError: Badge is not defined. Er

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,6 +12,7 @@ import { initiateAnonymousSignIn } from '@/firebase/non-blocking-login';
 import { useAuth } from '@/firebase';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
+import { Badge } from '@/components/ui/badge';
 
 export default function DashboardPage() {
   const { user, isUserLoading } = useUser();
